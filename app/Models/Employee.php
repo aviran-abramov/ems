dd838dbbad574b75b9c4cb73b3f92b4c798986f5
Aviran Abramov <aviranabramov@gmail.com>
2024-05-04 22:04:26 +0300

    Create store function to seed a new employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -18,4 +18,14 @@ class Employee extends Model
         'phone_number',
         'is_active'
     ];
+
+    public static function store($identity_number, $first_name, $last_name, $phone_number): Employee
+    {
+        return self::create([
+            'identity_number' => $identity_number,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'phone_number' => $phone_number
+        ]);
+    }
 }
