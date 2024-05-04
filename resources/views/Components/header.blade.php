33e2c5bc5af923e84c005355f48bf877853de4b9
Aviran Abramov <aviranabramov@gmail.com>
2024-05-04 14:30:34 +0300

    Create a nav link and routing to employees

--- resources/views/Components/header.blade.php
+++ resources/views/Components/header.blade.php
@@ -1,7 +1,9 @@
 <header>
     <nav class="bg-[#67258a] w-full h-24 flex items-center justify-center">
         <ul class="flex items-center">
-            
+            <li>
+                <a href="/employees" class="btn btn-ghost text-xl text-white font-semibold">Employees</a>
+            </li>
         </ul>
     </nav>
 </header>
